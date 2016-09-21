20821e21749a1bb2ac2f5fc52acffcdcb88397fb
Fortune Soleil <fsoleil@stuy.edu>
2016-09-20 23:33:29 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,11 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-@app.route("/memes")
-@app.route("/thanks")
 
 def hello_world():
     return "hello"
+
+@app.route("/memes")
+
+def goodbye_world():
+    return "goodbye"
+
+@app.route("/thanks")
+
+def thanks_world():
+    return "thanks"
 
 if __name__ == "__main__":
     app.run()
